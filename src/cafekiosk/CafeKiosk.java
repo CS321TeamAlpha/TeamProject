4f17a70a4384d5a90496d43459c654b660f0a3c7
Bernard <Bernard@Kirin>
2018-10-09 21:19:18 -0500

    minor update, nothing significant

--- src/cafekiosk/CafeKiosk.java
+++ src/cafekiosk/CafeKiosk.java
@@ -10,6 +10,13 @@
 *******************************************************************/
 package cafekiosk;
 
+import Model.Item;
+import Model.ItemManager;
+import Model.ItemBase;
+import Model.Menu;
+import Model.MenuItem;
+import Model.Store;
+import Model.StoreManager;
 import StateMachine.FSM;
 import StateMachine.StartState;
 import StateMachine.State;
@@ -25,16 +32,19 @@ import javafx.stage.Stage;
  * @author Bernard
  */
 public class CafeKiosk extends Application implements FSM {
+    private final ItemManager itemManager = ItemManager.get();
+    private final StoreManager storeManager = StoreManager.get();
     private final Stack<State> states = new Stack();
     private final StackPane root = new StackPane();
     
     @Override
     public void start(Stage primaryStage) {
+        GenerateTestData();
         pushState(new StartState());
                
         root.getChildren().add(getGUI());
         
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 600, 400);
         primaryStage.setTitle("Cafe Kiosk");
         primaryStage.setScene(scene);
         primaryStage.show();   
@@ -73,5 +83,38 @@ public class CafeKiosk extends Application implements FSM {
         }
         root.getChildren().clear();
         root.getChildren().add(getGUI());
+    }
+    
+    public void GenerateTestData(){
+        Store newStore1 = new Store();
+        newStore1.setName("Test Store 1");
+        Store newStore2 = new Store();
+        newStore2.setName("Test Store 2");
+        
+        Menu newMenu1 = new Menu();
+        newMenu1.setName("Test Menu 1");
+        newStore1.addMenu(newMenu1);
+        
+        Menu newMenu2 = new Menu();
+        newMenu2.setName("Test Menu 2");
+        newStore2.addMenu(newMenu2);
+        
+        ItemBase itemprof1 = new Item();
+        itemprof1.setName("Coffee");
+        itemprof1.setPrice(100);
+        ItemBase itemprof2 = new Item();
+        itemprof2.setName("Tea");
+        itemprof2.setPrice(100);
+        
+        MenuItem menuItem11 = new MenuItem(itemprof1);
+        MenuItem menuItem12 = new MenuItem(itemprof2);
+        MenuItem menuItem21 = new MenuItem(itemprof1);
+
+        newMenu1.add(menuItem11);
+        newMenu2.add(menuItem12);
+        newMenu2.add(menuItem21);
+        
+        StoreManager.get().add(newStore1);
+        StoreManager.get().add(newStore2);
     }
 }
